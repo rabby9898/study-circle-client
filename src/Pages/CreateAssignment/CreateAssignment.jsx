12b5af91ad7a9a0a7b85e345c3d67c6ca6ce97dc
rabby9898 <mdfajlerabby00@gmail.com>
2023-11-06 16:41:25 +0600

    create assignment functionality added

--- src/Pages/CreateAssignment/CreateAssignment.jsx
+++ src/Pages/CreateAssignment/CreateAssignment.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import Swal from "sweetalert2";
+import Swal from "sweetalert2";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+const backgroundImageUrl =
+  "url(https://i.ibb.co/T21Qgcr/back-school-witch-school-supplies.jpg)";
 
 const AddProduct = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const handleAddProduct = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -22,93 +24,98 @@ const AddProduct = () => {
     const difficulty = form.difficulty.value;
     const date = form.date.value;
     console.log(title, marks, thumbnail, imgUrl, difficulty, date, description);
-    // const products = {
-    //   imgUrl,
-    //   title,
-    //   marks,
-    //   description,
-    //   thumbnail,
-    //   difficulty,
-    //   date,
-    // };
+    const assignment = {
+      imgUrl,
+      title,
+      marks,
+      description,
+      thumbnail,
+      difficulty,
+      date,
+    };
 
-    // fetch(
-    //   "https://reeftech-server-h7d6u6kgy-fajle-rabbys-projects.vercel.app/products",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "content-type": "application/json",
-    //     },
-    //     body: JSON.stringify(products),
-    //   }
-    // )
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //     if (data.insertedId) {
-    //       Swal.fire({
-    //         title: "Success!",
-    //         text: "Your Product Added Successfully",
-    //         icon: "success",
-    //         confirmButtonText: "Cool",
-    //       });
-    //     }
-    //     navigate("/");
-    //   });
+    fetch("http://localhost:5000/assignments", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(assignment),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "Your Assignment Added Successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+        navigate("/");
+      });
   };
   return (
-    <div className="px-5 py-5 md:px-10 md:py-5 lg:px-36 lg:py-5">
+    <div
+      className="px-5 py-5 md:px-10 md:py-5 lg:px-36 lg:py-10"
+      style={{
+        backgroundImage: backgroundImageUrl,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
       <div className="mx-auto text-center">
         <h1 className="text-3xl text-black font-semibold">
           Create Your{" "}
           <span className="text-blue-600 font-light">Assignment</span>
         </h1>
       </div>
-      <div className="flex justify-center mx-auto">
+      <div className="flex justify-center mx-auto ">
         <form className="w-[600px]" onSubmit={handleAddProduct}>
           <label className="label">
-            <span className="label-text-base">Title</span>
+            <span className="label-text-lg font-semibold">Title</span>
           </label>
           <input
             type="text"
             placeholder="Title"
-            className="input input-bordered input-info w-full"
+            className="input input-bordered border-blue-500 focus:outline-blue-500 focus:border-0 w-full "
             name="title"
           />
           <label className="label">
-            <span className="label-text-base">Description</span>
+            <span className="label-text-lg font-semibold">Description</span>
           </label>
           <textarea
             type="text"
             placeholder="Short Description"
-            className="input input-bordered input-info w-full"
+            className="input input-bordered border-blue-500 focus:outline-blue-500 focus:border-0 w-full"
             name="description"
           />
           <label className="label">
-            <span className="label-text-base">Marks</span>
+            <span className="label-text-lg font-semibold">Marks</span>
           </label>
           <input
             type="text"
             placeholder="Marks"
-            className="input input-bordered input-info w-full"
+            className="input input-bordered border-blue-500 focus:outline-blue-500 focus:border-0 w-full"
             name="marks"
           />
           <label className="label">
-            <span className="label-text-base">Thumbnail URL</span>
+            <span className="label-text-lg font-semibold">Thumbnail URL</span>
           </label>
           <input
             type="text"
-            placeholder="Image URL"
-            className="input input-bordered input-info w-full"
+            placeholder="Thumbnail URL"
+            className="input input-bordered border-blue-500 focus:outline-blue-500 focus:border-0 w-full"
             name="thumbnail"
           />
           <label className="label">
-            <span className="label-text-base">Image URL</span>
+            <span className="label-text-lg font-semibold">Image URL</span>
           </label>
           <input
             type="text"
             placeholder="Image URL"
-            className="input input-bordered input-info w-full"
+            className="input input-bordered border-blue-500 focus:outline-blue-500 focus:border-0 w-full"
             name="imgUrl"
           />
           <div>
@@ -136,7 +143,7 @@ const AddProduct = () => {
           </div>
           <input
             type="submit"
-            className="btn input-info w-full mt-8 bg-[#00A3D1] uppercase text-white hover:bg-black hover:border hover:border-black"
+            className="btn input-info w-full mt-8 bg-blue-600 uppercase text-white hover:bg-black hover:border hover:border-black"
             value="Create Assignment"
           />
         </form>
