0d3b487a446c4045ccfe1adabd9d52ca5a0be8ab
rabby9898 <mdfajlerabby00@gmail.com>
2023-11-07 18:44:28 +0600

    banner section added

---
+++ src/Components/Feature/Feature.jsx
@@ -0,0 +1,78 @@
+const Feature = () => {
+  return (
+    <div className="px-2 md:px-10 lg:px-16 py-5 md:py-10">
+      <div className="mx-auto text-center my-10">
+        <h1 className="text-3xl capitalize text-blue-600 font-light">
+          Top Featured{" "}
+          <span className="text-black font-semibold">Assignments</span>
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <img
+            className="w-full"
+            src="https://i.ibb.co/RNLBWcr/calendar.jpg"
+            alt="Shoes"
+          />
+          <div className="px-9">
+            <h2 className="card-title my-3 text-black text-xl font-semibold">
+              History Research Paper
+            </h2>
+            <p className="text-black text-base my-3">Marks: 60/60</p>
+            <p className="bg-blue-600 text-white py-2 px-6 capitalize text-sm rounded-md my-3 max-w-fit">
+              Hard
+            </p>
+            <p className=" text-gray-600 text-base my-4">
+              Research and write a paper on a historical event or figure
+            </p>
+            <p className=" text-black capitalize">Date: 2023/10/17</p>
+          </div>
+        </div>
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <img
+            className="w-full"
+            src="https://i.ibb.co/RNLBWcr/calendar.jpg"
+            alt="Shoes"
+          />
+          <div className="px-9">
+            <h2 className="card-title my-3 text-black text-xl font-semibold">
+              History Research Paper
+            </h2>
+            <p className="text-black text-base my-3">Marks: 60/60</p>
+            <p className="bg-blue-600 text-white py-2 px-6 capitalize text-sm rounded-md my-3 max-w-fit">
+              Hard
+            </p>
+            <p className=" text-gray-600 text-base my-4">
+              Research and write a paper on a historical event or figure
+            </p>
+            <p className=" text-black capitalize">Date: 2023/10/17</p>
+          </div>
+        </div>
+
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <img
+            className="w-full"
+            src="https://i.ibb.co/RNLBWcr/calendar.jpg"
+            alt="Shoes"
+          />
+          <div className="px-9">
+            <h2 className="card-title my-3 text-black text-xl font-semibold">
+              History Research Paper
+            </h2>
+            <p className="text-black text-base my-3">Marks: 60/60</p>
+            <p className="bg-blue-600 text-white py-2 px-6 capitalize text-sm rounded-md my-3 max-w-fit">
+              Hard
+            </p>
+            <p className=" text-gray-600 text-base my-4">
+              Research and write a paper on a historical event or figure
+            </p>
+            <p className=" text-black capitalize pb-8">Date: 2023/10/17</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import Banner from "../../Components/Banner/Banner";
 import Faq from "../../Components/Faq/Faq";
+import Feature from "../../Components/Feature/Feature";
 import Footer from "../../Components/Footer/Footer";
 
 const Home = () => {
   return (
     <div>
+      <Banner></Banner>
+      <Feature></Feature>
       <Faq></Faq>
       <Footer></Footer>
     </div>
